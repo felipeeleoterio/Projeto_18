72294972fff3f92df86b08752e5264c2be12063d
felipeeleoterio <felipe.eleoterio@aluno.senai.br>
2024-11-06 21:54:43 -0300

    main

---
+++ src/main/java/com/mycompany/proejeto_18/Proejeto_18.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proejeto_18;
+
+/**
+ *
+ * @author f.silva
+ */
+public class Proejeto_18 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
